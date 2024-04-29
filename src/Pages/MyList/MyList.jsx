db08643e82403c6ea9c5987b55861faf1b8ee49a
TinnyDatta <tinnydatta.prof@gmail.com>
2024-04-29 13:17:10 +0600

    added mylist page

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -1,31 +1,48 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 
+
 const MyList = () => {
-    const {user} = useContext(AuthContext)
+    const {user} = useContext(AuthContext);
+    const [items, setItems] = useState([]);
+
     useEffect(() => {
         fetch(`http://localhost:5000/myList/${user?.email}`)
         .then(res=> res.json())
         .then(data => {
-            console.log(data)
+          setItems(data);
         })
-    } ,[user])
+    } ,[user]);
 
     return (
         <div>
-            <div className="overflow-x-auto my-12">
+<div className="overflow-x-auto my-12">
   <table className="table">
-    <tbody>
-       <tr>
-        <td>Brice Swyre <br />
-        Brice Swyre <br />
-        Brice Swyre 
-        </td>
-        {/* <button className="mt-10 mr-40">Update</button>
-        <button className="mt-10">Delete</button> */}
+    {/* head */}
+    <thead>
+      <tr className="text-xl">
+        <th>Country Name</th>
+        <th>Tourist Spot Name</th>
+        <th>Average Cost</th>
+      </tr>
+    </thead>
+     
+     {
+          items?.map(item  => ( 
+            
+            <tbody key={item._id}>
+      <tr className="font-medium my-2">
+        <td>{item.country}</td>
+        <td>{item.touristsSpot}</td>
+        <td>{item.averageCost}</td>
+        <td><button className="bg-purple-400 text-white p-2 rounded-lg">Update</button></td>
+        <td><button className="bg-purple-400 text-white p-2 rounded-lg">Delete</button></td>
       </tr>
     </tbody>
+          ))
+     }
+
   </table>
 </div>
         </div>
