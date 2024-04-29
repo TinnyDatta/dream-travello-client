294a00aed3961710bea406b7ed37ac055868e2be
TinnyDatta <tinnydatta.prof@gmail.com>
2024-04-29 12:25:53 +0600

    added light dark theme

--- src/Pages/MyList/MyList.jsx
+++ src/Pages/MyList/MyList.jsx
@@ -1,8 +1,33 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+
 
 const MyList = () => {
+    const {user} = useContext(AuthContext)
+    useEffect(() => {
+        fetch(`http://localhost:5000/myList/${user?.email}`)
+        .then(res=> res.json())
+        .then(data => {
+            console.log(data)
+        })
+    } ,[user])
+
     return (
         <div>
-            <h2>my lissst</h2>
+            <div className="overflow-x-auto my-12">
+  <table className="table">
+    <tbody>
+       <tr>
+        <td>Brice Swyre <br />
+        Brice Swyre <br />
+        Brice Swyre 
+        </td>
+        {/* <button className="mt-10 mr-40">Update</button>
+        <button className="mt-10">Delete</button> */}
+      </tr>
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const Navbar = () => {
   const {user, logout} = useContext(AuthContext)
+  const [theme, setTheme] = useState('light')
+
+  useEffect(()=>{
+ localStorage.setItem('theme', theme)
+ const localTheme = localStorage.getItem('theme')
+ document.querySelector('html').setAttribute('data-theme', localTheme)
+  },[theme])
+
+  const handleTheme = e => {
+    if(e.target.checked){
+      setTheme('dark')
+    }
+    else{
+      setTheme('light')
+    }
+  }
 
     const navLinks = <>
     <li><NavLink to='/' className={({isActive}) => isActive? ' text-purple-500  border border-purple-400 p-2' : ''}>Home</NavLink></li>
@@ -35,18 +51,7 @@ const Navbar = () => {
       {navLinks}
     </ul>
   </div>
-  {/* <div className="navbar-end">
-  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-        </div>
-      </div>
-    <div className="flex flex-row gap-2">
-        <Link to='/login' className="btn">Login</Link>
-        <Link to='/register' className="btn">Register</Link>
-    </div>
-    
-  </div> */}
+  
   <div className="navbar-end">
   {
     user?.email? 
@@ -66,6 +71,12 @@ const Navbar = () => {
   }
     
   </div>
+  {/* Theme controller */}
+  <label className="cursor-pointer grid place-items-center">
+  <input onChange={handleTheme} type="checkbox"  className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"/>
+  <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+  <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+</label>
 </div>
     );
 };
